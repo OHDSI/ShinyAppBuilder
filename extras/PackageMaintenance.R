a73b83510c54591abc7fb5171a2a65d29f411b81
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2023-02-08 01:13:04 -0500

    Modifications for inclusion in HADES

---
+++ extras/PackageMaintenance.R
@@ -0,0 +1,35 @@
+# Copyright 2023 Observational Health Data Sciences and Informatics
+#
+# This file is part of ShinyAppBuilder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Format and check code:
+styler::style_pkg()
+OhdsiRTools::checkUsagePackage("ShinyAppBuilder")
+OhdsiRTools::updateCopyrightYearFolder()
+OhdsiRTools::findNonAsciiStringsInFolder()
+devtools::spell_check()
+
+# Create manual and vignettes:
+unlink("extras/ShinyAppBuilder.pdf")
+shell("R CMD Rd2pdf ./ --output=extras/ShinyAppBuilder.pdf")
+
+rmarkdown::render("vignettes/shinyAppModules.Rmd",
+                  output_file = "../inst/doc/shinyAppModules.pdf",
+                  rmarkdown::pdf_document(latex_engine = "pdflatex",
+                                          toc = TRUE,
+                                          number_sections = TRUE))
+
+pkgdown::build_site()
+OhdsiRTools::fixHadesLogo()
