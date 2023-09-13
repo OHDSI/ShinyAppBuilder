1269abb0f64206eba46e91e372c011286a4e4a67
Jamie Gilbert <JGilber2@its.jnj.com>
2023-09-13 08:11:31 -0700

    Fix for broken code from merge

--- R/server.R
+++ R/server.R
@@ -14,119 +14,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-server <- function(config, connection, resultDatabaseSettings){
+server <- function(config, connection, resultDatabaseSettings) {
   return(
     shiny::shinyServer(function(input, output, session) {
-      
+
       # pointless code to use OhdsiShinyModules to prevent warning
       useless <- OhdsiShinyModules::getLogoImage()
-      
+
       #============= 
       # sidebar menu 
       #============= 
       output$sidebarMenu <- shinydashboard::renderMenu(
         do.call(
-          shinydashboard::sidebarMenu, 
+          shinydashboard::sidebarMenu,
           c(
-            lapply(config$shinyModules, function(module){
+            lapply(config$shinyModules, function(module) {
               addInfo(
                 item = shinydashboard::menuItem(
-                  text = module$tabText, 
-                  tabName = module$tabName, 
+                  text = module$tabText,
+                  tabName = module$tabName,
                   icon = shiny::icon(module$icon)
-                ), 
-                infoId = paste0(module$tabName,"Info")
+                ),
+                infoId = paste0(module$tabName, "Info")
               )
-            }
-            ), 
+            }),
             id = "menu"
           )
         )
       )
-    )
-  )
-  
-  
-  lapply(config$shinyModules, function(module){
-    if (!is.null(module$shinyModulePackage)) {
-      moduleInfoBox <- parse(text = paste0(module$shinyModulePackage, "::", module$infoBoxFile))
-    } else {
-      moduleInfoBox <- module$infoBoxFile
-    }
 
-    shiny::observeEvent(eval(parse(text = paste0('input$', module$tabName, 'Info'))), {
-      showInfoBox(module$tabName, eval(moduleInfoBox))
-    })
-  }
-  )
-  
-  # MODULE SERVERS HERE
-  runServer <- shiny::reactiveValues() 
-  for(module in  config$shinyModules){
-    runServer[[module$tabName]] <- 0
-  }
-  
-  shiny::observeEvent(input$menu,{ 
-    
-    runServer[[input$menu]] <- runServer[[input$menu]] +1 
-    
-    #lapply(config$shinyModules, function(module){
-            # run the server
-            tryCatch({
-              
-              shiny::withProgress({
-                do.call(
-                  what = eval(parse(text = paste0(module$shinyModulePackage, "::",module$serverFunction))),
-                  args = list(
-                    id = module$id,
-                    connectionHandler = connection,
-                    resultDatabaseSettings = resultDatabaseSettings
-                  )
-                )
-              }, message = paste("Loading module", module$moduleId)
-              )
-              
-            }, error = function (err) {
-              ParallelLogger::logError("Failed to load module ", module$tabName)
-              shiny::showNotification(
-                paste0("Error loading module: ", err),
-                type = "error"
-              )
-            }
-            )
-          }}
-        
+
+      lapply(config$shinyModules, function(module) {
+        if (!is.null(module$shinyModulePackage)) {
+          moduleInfoBox <- parse(text = paste0(module$shinyModulePackage, "::", module$infoBoxFile))
+        } else {
+          moduleInfoBox <- module$infoBoxFile
+        }
+
+        shiny::observeEvent(eval(parse(text = paste0('input$', module$tabName, 'Info'))), {
+          showInfoBox(module$tabName, eval(moduleInfoBox))
+        })
       })
-      
 
-  # HELPER FUNCTIONS
-  
-  addInfo <- function(item, infoId) {
-    infoTag <- shiny::tags$small(
-      class = "badge pull-right action-button",
-      style = "padding: 1px 6px 2px 6px; background-color: steelblue;",
-      type = "button", 
-      id = infoId,
-      "i"
-    )
-    item$children[[1]]$children <- append(item$children[[1]]$children, list(infoTag))
-    return(item)
-  }
-  
-  showInfoBox <- function(title, htmlFileName) { 
-    shiny::showModal(shiny::modalDialog( 
-      title = title, 
-      easyClose = TRUE, 
-      footer = NULL, 
-      size = "l", 
-      shiny::HTML(readChar(htmlFileName, file.info(htmlFileName)$size) ) 
-    )) 
-  }
-  
-})
+      # MODULE SERVERS HERE
+      runServer <- shiny::reactiveValues()
+      for (module in  config$shinyModules) {
+        runServer[[module$tabName]] <- 0
+      }
 
-)}
-  
+      shiny::observeEvent(input$menu, {
+
+        runServer[[input$menu]] <- runServer[[input$menu]] + 1
+
+        lapply(config$shinyModules, function(module) {
+          # run the server
+          tryCatch({
+
+            shiny::withProgress({
+              do.call(
+                what = eval(parse(text = paste0(module$shinyModulePackage, "::", module$serverFunction))),
+                args = list(
+                  id = module$id,
+                  connectionHandler = connection,
+                  resultDatabaseSettings = resultDatabaseSettings
+                )
+              )
+            }, message = paste("Loading module", module$moduleId)
+            )
+
+          }, error = function(err) {
+            ParallelLogger::logError("Failed to load module ", module$tabName)
+            shiny::showNotification(
+              paste0("Error loading module: ", err),
+              type = "error"
+            )
+          }
+          )
+        })
+
+      })
+    })
+  )
+}
+
+
+# HELPER FUNCTIONS
+
+addInfo <- function(item, infoId) {
+  infoTag <- shiny::tags$small(
+    class = "badge pull-right action-button",
+    style = "padding: 1px 6px 2px 6px; background-color: steelblue;",
+    type = "button",
+    id = infoId,
+    "i"
+  )
+  item$children[[1]]$children <- append(item$children[[1]]$children, list(infoTag))
+  return(item)
+}
+
+showInfoBox <- function(title, htmlFileName) {
+  shiny::showModal(shiny::modalDialog(
+    title = title,
+    easyClose = TRUE,
+    footer = NULL,
+    size = "l",
+    shiny::HTML(readChar(htmlFileName, file.info(htmlFileName)$size))
+  ))
+}
   
   
   
